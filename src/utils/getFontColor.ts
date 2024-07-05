f90fcc32f8a5c1f3632fe76de81d80ff26bd272b
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-07-05 11:18:26 +0000

    added getFontColor utils

---
+++ src/utils/getFontColor.ts
@@ -0,0 +1,11 @@
+import { ColorPalette } from "../styles";
+
+export const getFontColor = (backgroundColor: string): string => {
+    const hexColor = backgroundColor.replace("#", "");
+    const red = parseInt(hexColor.substr(0, 2), 16);
+    const green = parseInt(hexColor.substr(2, 2), 16);
+    const blue = parseInt(hexColor.substr(4, 2), 16);
+    const brightness = (red * 299 + green * 587 + blue * 114) / 1000;
+    return brightness > 125 ? ColorPalette.fontDark : ColorPalette.fontLight;
+  };
+  
