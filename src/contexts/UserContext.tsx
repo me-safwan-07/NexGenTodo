24d5c1e21afb95dd4ec126ec07793ebfe952adfa
Muhammed Safwan <mesafwan07@gmail.com>
2024-07-21 23:48:00 +0530

    add userContext file

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,15 +1,10 @@
-import React, { createContext } from "react";
-import type { User } from "../types/user";
+import { createContext } from "react";
+import { User } from "../types/user";
 import { defaultUser } from "../constants/defaultUser";
-import { useStorageState } from "../hooks/useStrorageState";
+
 interface UserProps {
-    user: User; // user data
+    user: User;    
     setUser: React.Dispatch<React.SetStateAction<User>>;
 }
 
-export const UserContext = createContext<UserProps>({ user: defaultUser, setUser: () => {}});
-
-export const UserContextProvider = ({ Children }: { Children: React.ReactNode }) => {
-    const [user, setUser] = useStorageState<User>(defaultUser, "user");
-    return <UserContext.Provider value={{ user, setUser }}>{Children}</UserContext.Provider>;
-};
+export const UserContext = createContext<UserProps>({ user: defaultUser, setUser: () => {} });
