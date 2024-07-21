a61c9171abb569dd6ae0bbc027b0e349704ed84a
Muhammed Safwan <mesafwan07@gmail.com>
2024-07-21 23:49:17 +0530

    add useResponsiveDisplay file

--- src/hooks/useResponsiveDisplay.ts
+++ src/hooks/useResponsiveDisplay.ts
@@ -1,16 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
-export  const useResponsiveDisplay = (breakpoint = 768): boolean => {
-    const [isSmallerDevice, setIsSmallerDevice] = useState<boolean>(false);
+export const useResponsiveDisplay = (breakpoint = 768): boolean => {
+    const [isSmallerDevice, setIsSmallerDevice] = useState<boolean>((false));
 
     useEffect(() => {
         const checkScreenSize = () => {
             setIsSmallerDevice(window.innerWidth < breakpoint);
         };
-        console.log(checkScreenSize());
+
+        checkScreenSize();
         const handleResize = () => checkScreenSize();
-        window.addEventListener('resize', handleResize);
-        return() => {
+        window.addEventListener("resize", handleResize);
+        return () => {
             window.removeEventListener("resize", handleResize);
         };
     }, [breakpoint]);
