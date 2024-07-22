46675b54960477e309c933740bce709fd2659c50
Muhammed Safwan <mesafwan07@gmail.com>
2024-07-22 22:23:26 +0530

    add MainLayout file

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import { ProfileSidebar } from "../components/Sidebar";
+import { BottomNav } from "../components/BottomNav";
+
+interface MainLayoutProps {
+    children: ReactNode;
+}
+
+const MainLayout = ({ children }: MainLayoutProps) => {
+    return(
+        <>
+            <ProfileSidebar />
+            {children}
+            <div style={{ marginTop: "128px"}}/>
+            <BottomNav />
+        </>
+    )
+};
+
+export default MainLayout;
