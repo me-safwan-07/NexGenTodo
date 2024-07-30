60d689e7eb05e0bda438cace257451c98ee67d76
Muhammed Safwan <mesafwan07@gmail.com>
2024-07-30 21:43:37 +0530

    Basic changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,15 @@
-import { Tooltip } from "@mui/material";
+import { Box, Tooltip, Typography } from "@mui/material";
 import { useResponsiveDisplay } from "../hooks/useResponsiveDisplay";
-import { AddButton } from "../styles";
+import { 
+    AddButton,
+    TasksCountContainer,
+    TasksCount,
+    StyledProgress,
+    ProgressPercentageContainer,
+    TaskCountTextContainer,
+    TaskCounterHeader,
+    TaskCompletionText,
+} from "../styles";
 import { useContext } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
@@ -15,9 +24,40 @@ const Home = () => {
     const isMobile = useResponsiveDisplay();
     return (
         <>
+            {tasks.length > 0 && (
+                <TasksCountContainer>
+                    <TasksCount >
+                        <Box >
+                            <StyledProgress 
+                                variant="determinate"
+                                size={64}
+                                thickness={5}
+                                aria-label="Progress"
+                            />
+
+                            <ProgressPercentageContainer>
+                                <Typography
+                                    variant="caption"
+                                    component="div"
+                                    color="white"
+                                    sx={{ fontSize: "16px", fontweight: 600 }}
+                                >
+                                    67%
+                                </Typography>
+                            </ProgressPercentageContainer>
+                        </Box>
+                        <TaskCountTextContainer>
+                            <TaskCounterHeader>
+                                You have 5 task
+                            </TaskCounterHeader>
+                        </TaskCountTextContainer>
+                        <TaskCompletionText>
+                            3
+                        </TaskCompletionText>
+                    </TasksCount>
+                </TasksCountContainer>
+            )}
             <TasksList />
-            
-            
             {!isMobile && (
                 <Tooltip title={tasks.length > 0 ? "Add New Task" : "Add Task"} placement="left">
                     <AddButton 
--- src/styles/home.styled.tsx
+++ src/styles/home.styled.tsx
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-import { Button, css } from "@mui/material";
+import { Box, Button, CircularProgress, css } from "@mui/material";
 import { pulseAnimation } from ".";
 import { ColorPalette } from "../theme/themeConfig";
 
@@ -29,4 +29,68 @@ export const AddButton = styled(Button)<{ animate?: boolean; glow: boolean }>`
         right: 24px;
     }
 
+`;
+
+export const TasksCountContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+export const TasksCount = styled.div`
+    color: purple;
+    background: #090b2258;
+    transition: 0.3s all;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    gap: 8px 16px;
+    padding: 20px 24px;
+    margin: 24px 0 12px 0;
+    border-radius: 24px;
+    width: 650px;
+    border: 1px solid purple;
+    @media (min-width: 1024px) {
+        padding: 24px;
+    }
+`;
+export const StyledProgress = styled(CircularProgress)`
+    z-index: 1;
+    margin: 2px;
+    filter: none;
+    transition: 0.3s filter;
+`;
+
+export const ProgressPercentageContainer = styled(Box)`
+    width: 50px;
+    height: 50px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #090b2287;
+    border-radius: 100px;
+    margin: -5px;
+    border: 1px solid purple;
+`;
+
+export const TaskCountTextContainer = styled.div`
+    line-height: 1.7;
+    margin-left: 6px;
+`;
+
+export const TaskCounterHeader = styled.h4`
+    margin: 0;
+    font-size: 16px;
+    @media (max-width: 1024px) {
+        font-size: 17px;
+    }
+`;
+
+export const TaskCompletionText  = styled.p`
+    margin: 0;
+    font-size: 16px;
 `
